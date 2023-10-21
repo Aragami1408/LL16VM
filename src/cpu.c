47d2c47ff5edd4ba31e801208c973e91418116b5
Aragami1408 <vucaominh1408@gmail.com>
2023-10-21 22:04:09 +1100

    Added new move, arithmetic, bitwise and branching instructions

--- src/cpu.c
+++ src/cpu.c
@@ -5,29 +5,29 @@
 
 cpu_t *cpu_init(memmapper_t *memory) {
 	cpu_t *cpu = (cpu_t *) malloc(sizeof(cpu_t));
-
+	
 	if(!cpu) {
 		exit(EXIT_FAILURE);
 	}
-
+	
 	for(int i = 0; i < CPU_REG_COUNT; i++) {
 		cpu->registers[i] = 0;
 	}
-
+	
 	// Loading instructions into the memory
 	// for(int i = 0; i < nbytes; i++) {
 	// 	cpu->memory[i] = writable_bytes[i];
 	// }
 	cpu->memory = memory;
-
+	
 	// cpu->registers[CPU_REG_SP] = ArrayCount(cpu->memory) - 1 - 1;
 	// cpu->registers[CPU_REG_FP] = ArrayCount(cpu->memory) - 1 - 1;
 	cpu->registers[CPU_REG_SP] = 0xffff - 1;
 	cpu->registers[CPU_REG_FP] = 0xffff - 1;
-
+	
 	cpu->stackframe_size = 0;
 	cpu->is_halt = false;
-
+	
 	return cpu;
 }
 
@@ -75,7 +75,7 @@ void cpu_stackPushState(cpu_t *cpu) {
 	cpu_stackPush(cpu, cpu->registers[CPU_REG_R8]);
 	cpu_stackPush(cpu, cpu->registers[CPU_REG_IP]);
 	cpu_stackPush(cpu, (u16) cpu->stackframe_size + 2);
-
+	
 	cpu->registers[CPU_REG_FP] = cpu->registers[CPU_REG_SP];
 	cpu->stackframe_size = 0;
 }
@@ -91,10 +91,10 @@ u16 cpu_stackPop(cpu_t *cpu) {
 void cpu_stackPopState(cpu_t *cpu) {
 	const u16 fp_address = cpu->registers[CPU_REG_FP];
 	cpu->registers[CPU_REG_SP] = fp_address;
-
+	
 	cpu->stackframe_size = cpu_stackPop(cpu);
 	const u16 stackframe_size = cpu->stackframe_size;
-
+	
 	cpu->registers[CPU_REG_IP] = cpu_stackPop(cpu);
 	cpu->registers[CPU_REG_R8] = cpu_stackPop(cpu);
 	cpu->registers[CPU_REG_R7] = cpu_stackPop(cpu);
@@ -104,12 +104,12 @@ void cpu_stackPopState(cpu_t *cpu) {
 	cpu->registers[CPU_REG_R3] = cpu_stackPop(cpu);
 	cpu->registers[CPU_REG_R2] = cpu_stackPop(cpu);
 	cpu->registers[CPU_REG_R1] = cpu_stackPop(cpu);
-
+	
 	const u16 nargs = cpu_stackPop(cpu);
 	for(int i = 0; i < nargs; i++) {
 		cpu_stackPop(cpu);
 	}
-
+	
 	cpu->registers[CPU_REG_FP] = fp_address + stackframe_size;
 }
 
@@ -134,13 +134,35 @@ void cpu_execute(cpu_t *cpu, u8 instruction) {
 		case MOV_MEM_REG: {
 			const u16 address = cpu_fetch16(cpu);
 			const u8 reg = cpu_fetch(cpu);
-
+			
 			// const u8 hi = cpu->memory[address];
 			// const u8 lo = cpu->memory[address + 1];
 			const u8 hi = memmapper_readU8(cpu->memory, address);
 			const u8 lo = memmapper_readU8(cpu->memory, address + 1);
 			cpu->registers[reg] = (u16) ((hi << 8) | (lo));
 		} break;
+		case MOV_LIT_MEM: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			memmapper_writeU16(cpu->memory, address, value);
+		} break;
+		case MOV_REG_PTR_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 ptr = cpu->registers[r1];
+			const u16 value = memmapper_readU16(cpu->memory, ptr);
+			cpu->registers[r2] = value;
+		} break;
+		case MOV_LIT_OFF_REG: {
+			const u16 base_address = cpu_fetch16(cpu);
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 offset = cpu->registers[r1];
+			
+			const u16 value = memmapper_readU16(cpu->memory, base_address + offset);
+			cpu->registers[r2] = value;
+		} break;
+		
 		case ADD_REG_REG: {
 			const u8 r1 = cpu_fetch(cpu);
 			const u8 r2 = cpu_fetch(cpu);
@@ -148,14 +170,250 @@ void cpu_execute(cpu_t *cpu, u8 instruction) {
 			const u16 reg_value2 = cpu->registers[r2];
 			cpu->registers[CPU_REG_ACC] = reg_value1 + reg_value2;
 		} break;
+		case ADD_LIT_REG: {
+			const u16 literal = cpu_fetch16(cpu);
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = literal + reg_value;
+		} break;
+		case SUB_LIT_REG: {
+			const u16 literal = cpu_fetch16(cpu);
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = reg_value - literal;
+		} break;
+		case SUB_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = literal - reg_value;
+		} break;
+		case SUB_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value1 = cpu->registers[r1];
+			const u16 reg_value2 = cpu->registers[r2];
+			cpu->registers[CPU_REG_ACC] = reg_value1 - reg_value2;
+		} break;
+		case MUL_LIT_REG: {
+			const u16 literal = cpu_fetch16(cpu);
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = literal * reg_value;
+		} break;
+		case MUL_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value1 = cpu->registers[r1];
+			const u16 reg_value2 = cpu->registers[r2];
+			cpu->registers[CPU_REG_ACC] = reg_value1 * reg_value2;
+		} break;
+		case INC_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 old_value = cpu->registers[r1];
+			const u16 new_value = old_value + 1;
+			cpu->registers[r1] = new_value;
+		} break;
+		case DEC_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 old_value = cpu->registers[r1];
+			const u16 new_value = old_value - 1;
+			cpu->registers[r1] = new_value;
+		} break;
+		
+		case LSF_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[r1] = reg_value << literal;
+		} break;
+		case LSF_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			const u16 shift_by = cpu->registers[r2];
+			cpu->registers[r1] = reg_value << shift_by;
+		} break;
+		case RSF_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[r1] = reg_value >> literal;
+		} break;
+		case RSF_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			const u16 shift_by = cpu->registers[r2];
+			cpu->registers[r1] = reg_value >> shift_by;
+		} break;
+		case AND_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = reg_value & literal;
+		} break;
+		case AND_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			const u16 shift_by = cpu->registers[r2];
+			cpu->registers[CPU_REG_ACC] = reg_value & shift_by;
+		} break;
+		case OR_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = reg_value | literal;
+		} break;
+		case OR_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			const u16 shift_by = cpu->registers[r2];
+			cpu->registers[CPU_REG_ACC] = reg_value | shift_by;
+		} break;
+		case XOR_REG_LIT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 literal = cpu_fetch16(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			cpu->registers[CPU_REG_ACC] = reg_value ^ literal;
+		} break;
+		case XOR_REG_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u8 r2 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			const u16 shift_by = cpu->registers[r2];
+			cpu->registers[CPU_REG_ACC] = reg_value ^ shift_by;
+		} break;
+		case NOT: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 reg_value = cpu->registers[r1];
+			
+			const u16 res = (~reg_value) & 0xffff;
+			cpu->registers[CPU_REG_ACC] = res;
+		} break;
+		
 		case JNE_LIT: {
 			const u16 value = cpu_fetch16(cpu);
 			const u16 address = cpu_fetch16(cpu);
-
+			
 			if (value != cpu->registers[CPU_REG_ACC]) {
 				cpu->registers[CPU_REG_IP] = address;
 			}
 		} break;
+		case JNE_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value != cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JNE_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value != cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JNE_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value != cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JEQ_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value == cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JEQ_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value == cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JLT_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value < cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JLT_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value < cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JGT_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value > cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JGT_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value > cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JLE_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value <= cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JLE_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value <= cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JGE_LIT: {
+			const u16 value = cpu_fetch16(cpu);
+			const u16 address = cpu_fetch16(cpu);
+			
+			if (value >= cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		case JGE_REG: {
+			const u8 r1 = cpu_fetch(cpu);
+			const u16 value = cpu->registers[r1];
+			const u16 address = cpu_fetch16(cpu);
+			
+			if(value >= cpu->registers[CPU_REG_ACC]) {
+				cpu->registers[CPU_REG_IP] = address;
+			}
+		} break;
+		
 		case PSH_LIT: {
 			const u16 value = cpu_fetch16(cpu);
 			cpu_stackPush(cpu, value);
@@ -168,6 +426,7 @@ void cpu_execute(cpu_t *cpu, u8 instruction) {
 			const u8 register_index = cpu_fetch(cpu);	
 			cpu->registers[register_index] = cpu_stackPop(cpu);
 		} break;
+		
 		case CAL_LIT: {
 			const u16 address = cpu_fetch16(cpu);
 			cpu_stackPushState(cpu);
@@ -182,6 +441,7 @@ void cpu_execute(cpu_t *cpu, u8 instruction) {
 		case RET: {
 			cpu_stackPopState(cpu);	
 		} break;
+		
 		case HLT: {
 			cpu->is_halt = true;
 		} break;
--- src/instructions.h
+++ src/instructions.h
@@ -7,10 +7,45 @@ static const u8 MOV_LIT_REG = 0x10;
 static const u8 MOV_REG_REG = 0x11;
 static const u8 MOV_REG_MEM = 0x12;
 static const u8 MOV_MEM_REG = 0x13;
+static const u8 MOV_LIT_MEM = 0x1B;
+static const u8 MOV_REG_PTR_REG = 0x1C;
+static const u8 MOV_LIT_OFF_REG = 0x1D;
 
 static const u8 ADD_REG_REG = 0x14;
+static const u8 ADD_LIT_REG = 0x3F;
+static const u8 SUB_LIT_REG = 0x16;
+static const u8 SUB_REG_LIT = 0x1E;
+static const u8 SUB_REG_REG = 0x1F;
+static const u8 INC_REG = 0x35;
+static const u8 DEC_REG = 0x36;
+static const u8 MUL_LIT_REG = 0x20;
+static const u8 MUL_REG_REG = 0x21;
+
+static const u8 LSF_REG_LIT = 0x26;
+static const u8 LSF_REG_REG = 0x27;
+static const u8 RSF_REG_LIT = 0x2A;
+static const u8 RSF_REG_REG = 0x2B;
+static const u8 AND_REG_LIT = 0x2E;
+static const u8 AND_REG_REG = 0x2F;
+static const u8 OR_REG_LIT = 0x30;
+static const u8 OR_REG_REG = 0x31;
+static const u8 XOR_REG_LIT = 0x32;
+static const u8 XOR_REG_REG = 0x33;
+static const u8 NOT = 0x34;
 
 static const u8 JNE_LIT = 0x15;
+static const u8 JNE_REG = 0x40;
+static const u8 JEQ_REG = 0x3E;
+static const u8 JEQ_LIT = 0x41;
+static const u8 JLT_LIT = 0x42;
+static const u8 JLT_REG = 0x43;
+static const u8 JGT_LIT = 0x44;
+static const u8 JGT_REG = 0x45;
+static const u8 JLE_LIT = 0x46;
+static const u8 JLE_REG = 0x47;
+static const u8 JGE_LIT = 0x48;
+static const u8 JGE_REG = 0x49;
+
 
 static const u8 PSH_LIT = 0x17;
 static const u8 PSH_REG = 0x18;
@@ -18,7 +53,6 @@ static const u8 POP = 0x1A;
 
 static const u8 CAL_LIT = 0x5E;
 static const u8 CAL_REG = 0x5F;
-
 static const u8 RET = 0x60;
 
 static const u8 HLT = 0xFF;
