5f96a08643fd7339a11010f55238aea103bf50c4
Aragami1408 <vucaominh1408@gmail.com>
2023-10-25 21:09:28 +1100

    Done parsing mov instructions

---
+++ src/assembler/parser.c
@@ -0,0 +1,118 @@
+#include "mpc.h"
+
+#ifdef _WIN32
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+void add_history(char* unused) {}	
+#else
+#include <editline/readline.h>
+#endif
+
+int main() {
+	
+	mpc_parser_t *assembler = mpc_new("assembler");
+
+	mpc_parser_t *mov = mpc_new("mov");
+
+	mpc_parser_t *lit= mpc_new("lit");
+	mpc_parser_t *lit_reg = mpc_new("lit_reg");
+	mpc_parser_t *reg_reg = mpc_new("reg_reg");
+	mpc_parser_t *reg_mem = mpc_new("reg_mem");
+	mpc_parser_t *mem_reg = mpc_new("mem_reg");
+	mpc_parser_t *lit_mem = mpc_new("lit_mem");
+	mpc_parser_t *reg_ptr_reg = mpc_new("reg_ptr_reg");
+	mpc_parser_t *lit_off_reg = mpc_new("lit_off_reg");
+
+	mpc_parser_t *hex_literal = mpc_new("hex_literal");
+	mpc_parser_t *address = mpc_new("address");
+
+	mpc_parser_t *square_bracket_expr = mpc_new("square_bracket_expr");
+	mpc_parser_t *expr = mpc_new("expr");
+	mpc_parser_t *term = mpc_new("term");
+	mpc_parser_t *factor = mpc_new("factor");
+
+	mpc_parser_t *variable = mpc_new("variable");
+
+	mpc_parser_t *registers = mpc_new("registers");
+	mpc_parser_t *gp_registers = mpc_new("gp_registers");
+
+	mpca_lang_contents(MPCA_LANG_DEFAULT, "assembler.grammar",
+					assembler,
+
+					mov,
+
+					lit,
+					lit_reg,
+					reg_reg,
+					reg_mem,
+					mem_reg,
+					lit_mem,
+					reg_ptr_reg,
+					lit_off_reg,
+
+					hex_literal,
+					address,
+
+					square_bracket_expr,
+					expr,
+					term,
+					factor,
+
+					variable,
+
+					registers,
+					gp_registers
+	);
+
+	while (1) {
+		char *input = readline("$ ");
+		add_history(input);
+
+		mpc_result_t r;
+		if (mpc_parse("stdin", input, assembler, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		}
+		else {
+		mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+
+		free(input);
+	}	
+		
+	mpc_delete(assembler);
+
+	mpc_delete(mov);
+
+	mpc_delete(lit);
+	mpc_delete(lit_reg);
+	mpc_delete(reg_reg);
+	mpc_delete(reg_mem);
+	mpc_delete(mem_reg);
+	mpc_delete(lit_mem);
+	mpc_delete(reg_ptr_reg);
+	mpc_delete(lit_off_reg);
+
+	mpc_delete(hex_literal);
+	mpc_delete(address);
+
+	mpc_delete(square_bracket_expr);
+	mpc_delete(expr);
+	mpc_delete(term);
+	mpc_delete(factor);
+
+	mpc_delete(variable);
+
+	mpc_delete(registers);
+	mpc_delete(gp_registers);
+}
