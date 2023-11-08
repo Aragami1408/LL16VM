055a05c9077348f4e6acb16d7f333cd2a9231608
Aragami1408 <vucaominh1408@gmail.com>
2023-11-08 14:07:56 +1100

    Done refactoring parsers

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -12,138 +12,173 @@
 
 #include "expr_parser.h"
 
+#include "common_parser.h"
+
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#endif
+
+int run_repl(mpc_parser_t *parser) {
+    while (1) {
+	char *input = readline("$ ");
+	add_history(input);
+	parser_instruction_t *inst = evaluate_instruction(parser, "<stdin>", input);
+
+	int machine_code[inst->type_size];	
+	for (int i = 0; i < inst->type_size; i++) {
+	    machine_code[i] = 0x00;
+	}
+
+	generate_machine_code(inst, machine_code);
+	printf("Machine Code: ");
+	for (int i = 0; i < inst->type_size; i++) {
+	    if (machine_code[i] == -1) printf("failed ");
+	    else printf("0x%02x ", machine_code[i]);
+	}
+
+	printf("\n\n");
+
+	free(inst);
+    }
+    return 0;
+}
 
 int run_file(mpc_parser_t *parser, const char *filename) {
 
-	int line_count;
-	char **lines = read_file_to_lines(filename, &line_count);
+    int line_count;
+    char **lines = read_file_to_lines(filename, &line_count);
 
-	if (lines != NULL) {
-		for (int i = 0; i < line_count; i++) {
+    if (lines != NULL) {
+	for (int i = 0; i < line_count; i++) {
 
-			if (strcmp(lines[i], "") == 0) continue;
-			printf("Line %d: %s\n", i + 1, lines[i]);
-			parser_instruction_t *inst = evaluate_instruction(parser, "input", lines[i]);
+	    if (strcmp(lines[i], "") == 0) continue;
+	    printf("Line %d: %s\n", i + 1, lines[i]);
+	    parser_instruction_t *inst = evaluate_instruction(parser, "input", lines[i]);
 
-			int machine_code[inst->type_size];	
-			for (int i = 0; i < inst->type_size; i++) {
-				machine_code[i] = 0x00;
-			}
+	    int machine_code[inst->type_size];	
+	    for (int i = 0; i < inst->type_size; i++) {
+		machine_code[i] = 0x00;
+	    }
 
-			generate_machine_code(inst, machine_code);
-			printf("Machine Code: ");
-			for (int i = 0; i < inst->type_size; i++) {
-				if (machine_code[i] == -1) printf("failed ");
-				else printf("0x%02x ", machine_code[i]);
-			}
+	    generate_machine_code(inst, machine_code);
+	    printf("Machine Code: ");
+	    for (int i = 0; i < inst->type_size; i++) {
+		if (machine_code[i] == -1) printf("failed ");
+		else printf("0x%02x ", machine_code[i]);
+	    }
 
-			printf("\n\n");
+	    printf("\n\n");
 
-			free(lines[i]); // Free the dynamically allocated memory for each line
-			free(inst);
-		}
-
-		free(lines);
+	    free(lines[i]); // Free the dynamically allocated memory for each line
+	    free(inst);
 	}
 
-	return 0;
+	free(lines);
+    }
+
+    return 0;
 }
 
 int main(int argc, char **argv) {
 
-	mpc_parser_t *assembler = mpc_new("assembler");
+    mpc_parser_t *assembler = mpc_new("assembler");
 
-	// Opcode parsers
-	opcode_parsers_t *opc_parsers = opcode_parsers_init();	
+    // Opcode parsers
+    opcode_parsers_t *opc_parsers = opcode_parsers_init();	
 
-	// Instruction type parsers
-	inst_type_parsers_t *inst_type_parsers = inst_type_parsers_init();
+    // Instruction type parsers
+    inst_type_parsers_t *inst_type_parsers = inst_type_parsers_init();
 
-	// Identifier parsers
-	mpc_parser_t *hex_literal = mpc_new("hex_literal");
-	mpc_parser_t *address = mpc_new("address");
+	// Common parsers
+	common_parsers_t *common_parsers = common_parsers_init();
 
-	// Square bracket expression parsers
-	expr_parser_t *expr_parser = expr_parser_init();	
 
-	mpc_parser_t *variable = mpc_new("variable");
+    // Square bracket expression parsers
+    expr_parser_t *expr_parser = expr_parser_init();	
 
-	// Register parsers
-	mpc_parser_t *registers = mpc_new("registers");
-	mpc_parser_t *gp_registers = mpc_new("gp_registers");
+    mpca_lang_contents(MPCA_LANG_DEFAULT, "assembler.grammar",
+	    assembler,
 
-	mpca_lang_contents(MPCA_LANG_DEFAULT, "assembler.grammar",
-			assembler,
+	    opc_parsers->mov,
+	    opc_parsers->add,
+	    opc_parsers->sub,
+	    opc_parsers->mul,
+	    opc_parsers->lsf,
+	    opc_parsers->rsf,
+	    opc_parsers->and,
+	    opc_parsers->or,
+	    opc_parsers->xor,
+	    opc_parsers->inc,
+	    opc_parsers->dec,
+	    opc_parsers->not,
+	    opc_parsers->jeq,
+	    opc_parsers->jne,
+	    opc_parsers->jlt,
+	    opc_parsers->jgt,
+	    opc_parsers->jle,
+	    opc_parsers->jge,
+	    opc_parsers->psh,
+	    opc_parsers->pop,
+	    opc_parsers->cal,
+	    opc_parsers->ret,
+	    opc_parsers->hlt,
 
-			opc_parsers->mov,
-			opc_parsers->add,
-			opc_parsers->sub,
-			opc_parsers->mul,
-			opc_parsers->lsf,
-			opc_parsers->rsf,
-			opc_parsers->and,
-			opc_parsers->or,
-			opc_parsers->xor,
-			opc_parsers->inc,
-			opc_parsers->dec,
-			opc_parsers->not,
-			opc_parsers->jeq,
-			opc_parsers->jne,
-			opc_parsers->jlt,
-			opc_parsers->jgt,
-			opc_parsers->jle,
-			opc_parsers->jge,
-			opc_parsers->psh,
-			opc_parsers->pop,
-			opc_parsers->cal,
-			opc_parsers->ret,
-			opc_parsers->hlt,
+	    inst_type_parsers->lit,
+	    inst_type_parsers->lit_reg,
+	    inst_type_parsers->reg_lit,
+	    inst_type_parsers->reg_reg,
+	    inst_type_parsers->reg_mem,
+	    inst_type_parsers->mem_reg,
+	    inst_type_parsers->lit_mem,
+	    inst_type_parsers->reg_ptr_reg,
+	    inst_type_parsers->lit_off_reg,
 
-			inst_type_parsers->lit,
-			inst_type_parsers->lit_reg,
-			inst_type_parsers->reg_lit,
-			inst_type_parsers->reg_reg,
-			inst_type_parsers->reg_mem,
-			inst_type_parsers->mem_reg,
-			inst_type_parsers->lit_mem,
-			inst_type_parsers->reg_ptr_reg,
-			inst_type_parsers->lit_off_reg,
+	    common_parsers->hex_literal,
+	    common_parsers->address,
 
-			hex_literal,
-			address,
+	    expr_parser->square_bracket_expr,
+	    expr_parser->expr,
+	    expr_parser->term,
+	    expr_parser->factor,
 
-			expr_parser->square_bracket_expr,
-			expr_parser->expr,
-			expr_parser->term,
-			expr_parser->factor,
+	    common_parsers->variable,
 
-			variable,
+	    common_parsers->registers,
+	    common_parsers->gp_registers
+		);
 
-			registers,
-			gp_registers
-				);
+    if (argc < 2) {
+	printf("Missing file. Going REPL mode\n");
+	run_repl(assembler);
+    }
+    else {
+	run_file(assembler, argv[1]);
+    }
 
-	if (argc < 2) {
-		printf("Missing argument: %s <asm_file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	else {
-		run_file(assembler, argv[1]);
-	}
+    mpc_delete(assembler);
 
-	mpc_delete(assembler);
+    opcode_parsers_free(opc_parsers);
 
-	opcode_parsers_free(opc_parsers);
+    inst_type_parsers_free(inst_type_parsers);
 
-	inst_type_parsers_free(inst_type_parsers);
+	common_parsers_free(common_parsers);
 
-	mpc_delete(hex_literal);
-	mpc_delete(address);
+    expr_parser_free(expr_parser);
 
-	expr_parser_free(expr_parser);
 
-	mpc_delete(variable);
-
-	mpc_delete(registers);
-	mpc_delete(gp_registers);
 }
---
+++ src/assembler/common_parser.c
@@ -0,0 +1,25 @@
+#include "common_parser.h"
+
+common_parsers_t *common_parsers_init(void) {
+	common_parsers_t *common_parsers = (common_parsers_t *) malloc(sizeof(common_parsers_t));
+
+	common_parsers->hex_literal = mpc_new("hex_literal");
+	common_parsers->address = mpc_new("address");
+	common_parsers->registers = mpc_new("registers");
+	common_parsers->gp_registers = mpc_new("gp_registers");
+	common_parsers->variable = mpc_new("variable");
+	
+
+	return common_parsers;
+}
+
+void common_parsers_free(common_parsers_t *common_parsers) {
+	
+	mpc_delete(common_parsers->hex_literal);
+	mpc_delete(common_parsers->address);
+	mpc_delete(common_parsers->registers);
+	mpc_delete(common_parsers->gp_registers);
+	mpc_delete(common_parsers->variable);
+
+	free(common_parsers);
+}
